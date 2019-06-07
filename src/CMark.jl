e97b306de68c5b93e20c976c4c75cd02d883b332
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-06-07 16:32:18 +1200

    Add a function to parse CommonMark to Julia MD AST

--- src/CMark.jl
+++ src/CMark.jl
@@ -1,8 +1,58 @@
 module CMark
+import Markdown
 
 const libcmark = joinpath(@__DIR__, "..", "deps", "libcmark.so")
 
 const CMARK_OPT_DEFAULT = 0
+
+@enum cmark_node_type begin
+    # Error status
+    CMARK_NODE_NONE
+
+    # Block
+    CMARK_NODE_DOCUMENT
+    CMARK_NODE_BLOCK_QUOTE
+    CMARK_NODE_LIST
+    CMARK_NODE_ITEM
+    CMARK_NODE_CODE_BLOCK
+    CMARK_NODE_HTML_BLOCK
+    CMARK_NODE_CUSTOM_BLOCK
+    CMARK_NODE_PARAGRAPH
+    CMARK_NODE_HEADING
+    CMARK_NODE_THEMATIC_BREAK
+
+    # CMARK_NODE_FIRST_BLOCK = CMARK_NODE_DOCUMENT
+    # CMARK_NODE_LAST_BLOCK = CMARK_NODE_THEMATIC_BREAK
+
+    # Inline
+    CMARK_NODE_TEXT
+    CMARK_NODE_SOFTBREAK
+    CMARK_NODE_LINEBREAK
+    CMARK_NODE_CODE
+    CMARK_NODE_HTML_INLINE
+    CMARK_NODE_CUSTOM_INLINE
+    CMARK_NODE_EMPH
+    CMARK_NODE_STRONG
+    CMARK_NODE_LINK
+    CMARK_NODE_IMAGE
+
+    # CMARK_NODE_FIRST_INLINE = CMARK_NODE_TEXT
+    # CMARK_NODE_LAST_INLINE = CMARK_NODE_IMAGE
+end
+
+@enum cmark_list_type begin
+    CMARK_NO_LIST
+    CMARK_BULLET_LIST
+    CMARK_ORDERED_LIST
+end
+# } cmark_list_type;
+#
+# typedef enum {
+# CMARK_NO_DELIM,
+# CMARK_PERIOD_DELIM,
+# CMARK_PAREN_DELIM
+# } cmark_delim_type;
+
 
 function cmark_version()
     ccall((:cmark_version, libcmark), Cint, ()) |> Int
@@ -12,5 +62,320 @@ function cmark_version_string()
     s = ccall((:cmark_version_string, libcmark), Cstring, ())
     unsafe_string(s)
 end
+
+function cmark_markdown_to_html(markdown::AbstractString)
+    s = ccall(
+        (:cmark_markdown_to_html, libcmark),
+        Cstring, (Cstring, Csize_t, Cint),
+        markdown, length(markdown), CMARK_OPT_DEFAULT
+    )
+    unsafe_string(s)
+end
+
+function cmark_parse_document(markdown::AbstractString)
+    ccall(
+        (:cmark_parse_document, libcmark),
+        Ptr{Cvoid}, (Cstring, Csize_t, Cint),
+        markdown, length(markdown), CMARK_OPT_DEFAULT
+    )
+end
+
+"""
+    cmark_node_get_type(node::Ptr{Cvoid}) -> node_type::Cuint
+
+Wraps `cmark_node_get_type` from `libcmark`:
+
+> `cmark_node_type cmark_node_get_type(cmark_node *node)`
+>
+> Returns the type of node, or `CMARK_NODE_NONE` on error.
+"""
+function cmark_node_get_type(node::Ptr{Cvoid})
+    t = ccall(
+        (:cmark_node_get_type, libcmark),
+        Cuint, (Ptr{Cvoid},),
+        node
+    )
+end
+
+"""
+    cmark_node_next(node::Ptr{Cvoid}) -> node::Ptr{Cvoid}
+
+Wraps `cmark_node_next` from `libcmark`:
+
+> `cmark_node * cmark_node_next(cmark_node *node)`
+>
+> Returns the next node in the sequence after `node`, or `NULL` if there is none.
+"""
+function cmark_node_next(node::Ptr{Cvoid})
+    ccall(
+        (:cmark_node_next, libcmark),
+        Ptr{Cvoid}, (Ptr{Cvoid},),
+        node
+    )
+end
+
+"""
+    cmark_node_first_child(node::Ptr{Cvoid}) -> node::Ptr{Cvoid}
+
+Wraps `cmark_node_first_child` from `libcmark`:
+
+> `cmark_node * cmark_node_first_child(cmark_node *node)`
+>
+> Returns the first child of `node`, or `NULL` if `node` has no children.
+"""
+function cmark_node_first_child(node::Ptr{Cvoid})
+    ccall(
+        (:cmark_node_first_child, libcmark),
+        Ptr{Cvoid}, (Ptr{Cvoid},),
+        node
+    )
+end
+
+"""
+    cmark_node_get_literal(node::Ptr{Cvoid}) -> s::Cstring
+
+Wraps `cmark_node_get_literal` from `libcmark`:
+
+> `const char * cmark_node_get_literal(cmark_node *node)`
+>
+> Returns the string contents of `node`, or an empty string if none is set. Returns `NULL` if
+> called on a node that does not have string content.
+"""
+function cmark_node_get_literal(node::Ptr{Cvoid})
+    ccall(
+        (:cmark_node_get_literal, libcmark),
+        Cstring, (Ptr{Cvoid},),
+        node
+    )
+end
+
+"""
+   cmark_node_get_heading_level(node::Ptr{Cvoid}) -> level::Cint
+
+Wraps `cmark_node_get_heading_level` from `libcmark`:
+
+> `int cmark_node_get_heading_level(cmark_node *node)`
+>
+> Returns the heading level of `node`, or `0` if `node` is not a heading.
+"""
+function cmark_node_get_heading_level(node::Ptr{Cvoid})
+   ccall(
+       (:cmark_node_get_heading_level, libcmark),
+       Cint, (Ptr{Cvoid},),
+       node
+   )
+end
+
+"""
+   cmark_node_get_fence_info(node::Ptr{Cvoid}) -> Cstring
+
+Wraps `cmark_node_get_fence_info` from `libcmark`:
+
+> `const char * cmark_node_get_fence_info(cmark_node *node)`
+>
+> Returns the info string from a fenced code block.
+"""
+function cmark_node_get_fence_info(node::Ptr{Cvoid})
+   ccall(
+       (:cmark_node_get_fence_info, libcmark),
+       Cstring, (Ptr{Cvoid},),
+       node
+   )
+end
+
+"""
+   cmark_node_get_list_type(node::Ptr{Cvoid}) -> cmark_list_type
+
+Wraps `cmark_node_get_list_type` from `libcmark`:
+
+> `cmark_list_type cmark_node_get_list_type(cmark_node *node)`
+>
+> Returns the list type of `node`, or `CMARK_NO_LIST` if `node` is not a list.
+"""
+function cmark_node_get_list_type(node::Ptr{Cvoid})
+   ccall(
+       (:cmark_node_get_list_type, libcmark),
+       Cuint, (Ptr{Cvoid},),
+       node
+   )
+end
+
+"""
+   cmark_node_get_url(node::Ptr{Cvoid}) -> cmark_list_type
+
+Wraps `cmark_node_get_url` from `libcmark`:
+
+> `const char * cmark_node_get_url(cmark_node *node)`
+>
+> Returns the URL of a link or image node, or an empty string if no URL is set. Returns
+> `NULL` if called on a node that is not a link or image.
+"""
+function cmark_node_get_url(node::Ptr{Cvoid})
+   ccall(
+       (:cmark_node_get_url, libcmark),
+       Cstring, (Ptr{Cvoid},),
+       node
+   )
+end
+
+"""
+    cmark_node_get_title(node::Ptr{Cvoid}) -> Cstring
+
+Wraps `cmark_node_get_title` from `libcmark`:
+
+> `const char * cmark_node_get_title(cmark_node *node)`
+>
+> Returns the title of a link or image node, or an empty string if no title is set. Returns
+> `NULL` if called on a node that is not a link or image.
+"""
+function cmark_node_get_title(node::Ptr{Cvoid})
+   ccall(
+       (:cmark_node_get_title, libcmark),
+       Cstring, (Ptr{Cvoid},),
+       node
+   )
+end
+
+
+# Public Julia API
+struct CMarkNode
+    p :: Ptr{Cvoid}
+end
+
+parse_document(markdown::AbstractString) = CMarkNode(cmark_parse_document(markdown))
+
+function nodetype(node::CMarkNode)
+    cmark_node_type(cmark_node_get_type(node.p))
+end
+
+function listtype(node::CMarkNode)
+    cmark_list_type(cmark_node_get_list_type(node.p))
+end
+
+function linkinfo(node::CMarkNode)
+    url = cmark_node_get_url(node.p)
+    title = cmark_node_get_title(node.p)
+    (
+        url == C_NULL ? nothing : unsafe_string(url),
+        title == C_NULL ? nothing : unsafe_string(title)
+    )
+end
+
+function Base.first(node::CMarkNode)
+    p = cmark_node_first_child(node.p)
+    p == C_NULL ? nothing : CMarkNode(p)
+end
+
+function next(node::CMarkNode)
+    p = cmark_node_next(node.p)
+    p == C_NULL ? nothing : CMarkNode(p)
+end
+
+function heading_level(node::CMarkNode)
+    level = cmark_node_get_heading_level(node.p)
+    iszero(level) ? nothing : Int(level)
+end
+
+function literal(node::CMarkNode)
+    s = cmark_node_get_literal(node.p)
+    s == C_NULL ? nothing : unsafe_string(s)
+end
+
+function code_fence_info(node::CMarkNode)
+    s = cmark_node_get_fence_info(node.p)
+    s == C_NULL ? nothing : unsafe_string(s)
+end
+
+# Convert to Base Markdown
+function markdown_withsiblings(node::Union{CMarkNode,Nothing})
+    nodes = Any[]
+    n = node
+    while !isnothing(n)
+        push!(nodes, markdown(n))
+        n = next(n)
+    end
+    return nodes
+end
+
+function markdown(node::CMarkNode)
+    ntype = nodetype(node)
+    if ntype == CMARK_NODE_DOCUMENT
+        Markdown.MD(markdown_withsiblings(first(node)))
+    elseif ntype == CMARK_NODE_BLOCK_QUOTE
+        Markdown.BlockQuote(markdown_withsiblings(first(node)))
+    elseif ntype == CMARK_NODE_LIST
+        # TODO: implement cmark_node_get_list_start
+        # TODO: implement cmark_node_get_list_tight
+        list_type = listtype(node)
+        items = markdown_withsiblings(first(node))
+        if list_type == CMARK_BULLET_LIST
+            Markdown.List(items)
+        elseif list_type == CMARK_ORDERED_LIST
+            Markdown.List(items, 1)
+        else
+            error("Bad list type $(list_type)")
+        end
+    elseif ntype == CMARK_NODE_ITEM
+        markdown_withsiblings(first(node))
+    elseif ntype == CMARK_NODE_CODE_BLOCK
+        Markdown.Code(code_fence_info(node), literal(node))
+    elseif ntype == CMARK_NODE_HTML_BLOCK
+        Markdown.Code("@raw html", literal(node))
+    # elseif ntype == CMARK_NODE_CUSTOM_BLOCK
+    elseif ntype == CMARK_NODE_PARAGRAPH
+        Markdown.Paragraph(markdown_withsiblings(first(node)))
+    elseif ntype == CMARK_NODE_HEADING
+        level = heading_level(node)
+        Markdown.Header(markdown_withsiblings(first(node)), level)
+    elseif ntype == CMARK_NODE_THEMATIC_BREAK
+        Markdown.HorizontalRule()
+    # # Inline
+    elseif ntype == CMARK_NODE_TEXT
+        literal(node)
+    elseif ntype == CMARK_NODE_SOFTBREAK
+        " "
+    elseif ntype == CMARK_NODE_LINEBREAK
+        # TODO: not really supported by Base Markdown?
+        "\n"
+    elseif ntype == CMARK_NODE_CODE
+        Markdown.Code(literal(node))
+    elseif ntype == CMARK_NODE_HTML_INLINE
+        Markdown.Code("@raw html", literal(node))
+    # elseif ntype == CMARK_NODE_CUSTOM_INLINE
+    elseif ntype == CMARK_NODE_EMPH
+        Markdown.Italic(markdown_withsiblings(first(node)))
+    elseif ntype == CMARK_NODE_STRONG
+        Markdown.Bold(markdown_withsiblings(first(node)))
+    elseif ntype == CMARK_NODE_LINK
+        url, _ = linkinfo(node) # we're discarding the title
+        Markdown.Link(markdown_withsiblings(first(node)), url)
+    elseif ntype == CMARK_NODE_IMAGE
+        url, _ = linkinfo(node) # we're discarding the title
+        # TODO: we will also discard the alt text, since Julia only supports string, but
+        # CommonMark allows for arbitrary formatting.
+        Markdown.Image(url, "")
+    else
+        error("Node type $(ntype) not implemented")
+    end
+end
+
+markdown(s::AbstractString) = markdown(parse_document(s))
+
+function typetree(node::CMarkNode; level=0)
+    n = node
+    while !isnothing(n)
+        c = first(n)
+        if isnothing(c)
+            println("\t"^level, nodetype(n))
+        else
+            println("\t"^level, nodetype(n), " => {")
+            typetree(c, level = level + 1)
+            println("\t"^level, "}")
+        end
+        n = next(n)
+    end
+end
+
+export markdown
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,250 @@
 using CMark
 using Test
+import Markdown
 
 @testset "CMark.jl" begin
     # Write your own tests here.
     @test CMark.cmark_version() == 7424
     @test CMark.cmark_version_string() == "0.29.0"
+
+    # simple empty Markdown document
+    let md = "" |> CMark.parse_document |> CMark.markdown_withsiblings
+        @test length(md) == 1
+        @test md[1] isa Markdown.MD
+        @test isempty(md[1])
+    end
+
+    # single string
+    let md = "foo" |> CMark.parse_document |> CMark.markdown_withsiblings
+        @test length(md) == 1
+        @test md[1] isa Markdown.MD
+        @test length(md[1]) == 1
+        @test md[1][1] isa Markdown.Paragraph
+        @test length(md[1][1].content) == 1
+        @test md[1][1].content[1] == "foo"
+    end
+
+    # some basic inline elements
+    let md = "foo **strong** _emphasis_" |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 1
+        @test md[1] isa Markdown.Paragraph
+        @test length(md[1].content) == 4
+        @test md[1].content[1] == "foo "
+        @test md[1].content[2] isa Markdown.Bold
+        @test md[1].content[3] == " "
+        @test md[1].content[4] isa Markdown.Italic
+    end
+
+    # Headings and inline code
+    let md = """
+        # Heading `1`
+        ## Heading `2`
+        ### Heading `3`
+        #### Heading `4`
+        ##### Heading `5`
+        ###### Heading `6`
+
+        Setext `1`
+        ==========
+
+        Setext `2`
+        ----------
+        """ |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 8
+        for h in md.content
+            @test h isa Markdown.Header
+            @test length(h.text) == 2
+            @test h.text[2] isa Markdown.Code
+            level = parse(Int, h.text[2].code)
+            @test h isa Markdown.Header{level}
+        end
+    end
+
+    # code, blockquote and thematic break blocks
+    let md = """
+        ```foo bar baz
+        code
+        example()
+        ```
+        ---
+        > Paragraph 1
+        ---
+        > Paragraph 1
+        >
+        > Paragraph 2
+        ---
+            Indented code
+        """ |> CMark.parse_document |> CMark.markdown
+        @test md isa Markdown.MD
+        @test length(md) == 7
+        @test md[1] isa Markdown.Code
+        @test md[1].language == "foo bar baz"
+        @test md[1].code == "code\nexample()\n"
+        @test md[2] isa Markdown.HorizontalRule
+        @test md[3] isa Markdown.BlockQuote
+        @test length(md[3].content) == 1
+        @test md[4] isa Markdown.HorizontalRule
+        @test md[5] isa Markdown.BlockQuote
+        @test length(md[5].content) == 2
+        @test md[5].content[1] isa Markdown.Paragraph
+        @test md[6] isa Markdown.HorizontalRule
+        @test md[7] isa Markdown.Code
+        @test md[7].language == ""
+        @test md[7].code == "Indented code\n"
+    end
+
+    # lists
+    let md = """
+        * A
+        * B
+        ---
+        * A
+
+          B
+        * C
+        ---
+        1. Foo
+        2. Bar
+        """ |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 5
+
+        @test md[1] isa Markdown.List
+        @test md[1].ordered == -1
+        @test length(md[1].items) == 2
+        @test md[1].items[1] isa Vector
+        @test length(md[1].items[1]) == 1
+        @test md[1].items[1][1] isa Markdown.Paragraph
+
+        let list = md[3]
+            @test list isa Markdown.List
+            @test list.ordered == -1
+            @test length(list.items) == 2
+            @test list.items[1] isa Vector
+            @test length(list.items[1]) == 2
+            @test list.items[1][1] isa Markdown.Paragraph
+            @test list.items[1][2] isa Markdown.Paragraph
+            @test list.items[2] isa Vector
+            @test length(list.items[2]) == 1
+            @test list.items[2][1] isa Markdown.Paragraph
+        end
+
+        let list = md[5]
+            @test list isa Markdown.List
+            @test list.ordered == 1
+            @test length(list.items) == 2
+            @test list.items[1] isa Vector
+            @test length(list.items[1]) == 1
+            @test list.items[1][1] isa Markdown.Paragraph
+            @test list.items[2] isa Vector
+            @test length(list.items[2]) == 1
+            @test list.items[2][1] isa Markdown.Paragraph
+        end
+    end
+
+    # linebreaks
+    let md = "Soft\nBreak\n\n---\n\nHard  \nBreak\n" |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 3
+        let p = md[1]
+            @test p isa Markdown.Paragraph
+            @test length(p.content) == 3
+            @test p.content[1] == "Soft"
+            @test p.content[2] == " "
+            @test p.content[3] == "Break"
+        end
+        let p = md[3]
+            @test p isa Markdown.Paragraph
+            @test length(p.content) == 3
+            @test p.content[1] == "Hard"
+            @test p.content[2] == "\n"
+            @test p.content[3] == "Break"
+        end
+    end
+
+    # links and link definitions
+    let md = """
+        [link text](url)
+
+        [link text](url "title")
+
+        <http://foo.bar.baz>
+
+        [text][link1]
+
+        [text][link2]
+
+        [link1]: foo
+        [link2]: bar "title"
+        """ |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 5
+        @test all(isa.(md.content, Markdown.Paragraph))
+        @test all(length(p.content) == 1 for p in md.content)
+
+        let link = md[1].content[1]
+            @test link isa Markdown.Link
+            @test link.text == ["link text"]
+            @test link.url == "url"
+        end
+        let link = md[2].content[1]
+            @test link isa Markdown.Link
+            @test link.text == ["link text"]
+            @test link.url == "url"
+        end
+        let link = md[3].content[1]
+            @test link isa Markdown.Link
+            @test link.text == ["http://foo.bar.baz"]
+            @test link.url == "http://foo.bar.baz"
+        end
+        let link = md[4].content[1]
+            @test link isa Markdown.Link
+            @test link.text == ["text"]
+            @test link.url == "foo"
+        end
+        let link = md[5].content[1]
+            @test link isa Markdown.Link
+            @test link.text == ["text"]
+            @test link.url == "bar"
+        end
+    end
+
+    # image
+    let md = "![alt text](url \"title\")" |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 1
+        @test md[1] isa Markdown.Paragraph
+        let image = md[1].content[1]
+            @test image isa Markdown.Image
+            @test image.url == "url"
+            @test image.alt == ""
+        end
+    end
+
+    # HTML
+    let md = """
+        Inline <span>HTML</span>!
+
+        <div>HTML blocks</div>
+        """ |> markdown
+        @test md isa Markdown.MD
+        @test length(md) == 2
+        let p = md[1]
+            @test p isa Markdown.Paragraph
+            @test length(p.content) == 5
+            @test p.content[2] isa Markdown.Code
+            @test p.content[2].language == "@raw html"
+            @test p.content[2].code == "<span>"
+            @test p.content[4] isa Markdown.Code
+            @test p.content[4].language == "@raw html"
+            @test p.content[4].code == "</span>"
+        end
+        let code = md[2]
+            @test code isa Markdown.Code
+            @test code.language == "@raw html"
+            @test code.code == "<div>HTML blocks</div>\n"
+        end
+    end
 end
